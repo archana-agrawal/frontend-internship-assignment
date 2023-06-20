3a118d0f02e1d0d1b29c2953c612123dc4da37d0
Archana Agrawal <69050324+archana-agrawal@users.noreply.github.com>
2023-06-20 14:30:57 +0530

    Update issues-test.java

--- e2e/src/support/issues-test.java
+++ e2e/src/support/issues-test.java
@@ -1,1 +1,32 @@
+public class AnalyticServiceChecker {
+    public static boolean isAccountDataSent(String accountID, String accountName) {
+        // Replace the code below with the appropriate API or SDK calls to the analytic service
 
+        // Example logic:
+        // Make an API call or query the analytic service to check if the account data exists
+        // Return true if the account data is found, otherwise return false
+
+        // For demonstration purposes, assume the analytic service stores the account data in a database
+        // Check if the account data exists in the database
+        boolean accountDataExists = checkAccountDataInDatabase(accountID, accountName);
+
+        return accountDataExists;
+    }
+
+    private static boolean checkAccountDataInDatabase(String accountID, String accountName) {
+        // Implement the database query logic to check if the account data exists
+        // Return true if the account data is found, otherwise return false
+        // This is just a placeholder implementation
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String accountID = "123456";
+        String accountName = "Example Account";
+
+        boolean accountDataSent = isAccountDataSent(accountID, accountName);
+
+        if (accountDataSent) {
+            System.out.println("Account data has been sent to the analytic service.");
+        } else {
+           
